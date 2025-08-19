3f71e4f3684ececc0a5aba0fba34ac074595c5b3
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-19 19:34:33 +0530

    migration-file

---
+++ db/migrate/-seq/20250819135734_init_schema.up.sql
@@ -0,0 +1,33 @@
+-- Accounts table
+CREATE TABLE accounts (
+    id BIGSERIAL PRIMARY KEY,
+    owner VARCHAR NOT NULL,
+    balance BIGINT NOT NULL,
+    currency VARCHAR NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX ON accounts (owner);
+
+-- Entries table
+CREATE TABLE entries (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL REFERENCES accounts(id),
+    amount BIGINT NOT NULL, -- can be negative or positive
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX ON entries (account_id);
+
+-- Transfers table
+CREATE TABLE transfers (
+    id BIGSERIAL PRIMARY KEY,
+    from_account_id BIGINT NOT NULL REFERENCES accounts(id),
+    to_account_id BIGINT NOT NULL REFERENCES accounts(id),
+    amount BIGINT NOT NULL, -- must be positive
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX ON transfers (from_account_id);
+CREATE INDEX ON transfers (to_account_id);
+CREATE INDEX ON transfers (from_account_id, to_account_id);

55eefe6ac855d9b21d17195a60c4d3cd1d529117
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-24 13:34:13 +0530

    db update for trnscition

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -36,8 +36,61 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error{
 return	tx.Commit()
 }
 
-
+// all input params
 type TransferTxParams struct{
- FromAccount
+ FromAccountID int64 `json:"from_account_id"`
+ ToAccountID int64     `json:"to_account_id"`
+ Amount        int64     `json:"amount"`
+
 }
-func (store *Store) TransferTx(ctx context.Context, arg Transfer)
+
+
+// contains the result of transfer transcition
+type TransferTxResult struct{
+	Transfer Transfer `josn:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account `json:"to_account"`
+	FromEntry    Entry  `json:"from_entry"`
+	ToEntry		Entry	`json:"to_entry"`
+
+}
+
+
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams)(TransferTxResult,error){
+	var result TransferTxResult
+	err:=store.execTx(ctx,func(q *Queries) error{
+		var err error
+		result.Transfer,err:=q.CreateTransfer(ctx,CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+
+
+
+
+		})
+		if err !=nil{
+			return err
+		}
+		result.FromEntry,err:=q.CreateEntry(ctx,CreateEntryParams{
+			AccountID:arg.FromAccountID ,
+			Amount: arg.Amount,
+		})
+		if err!=nil{
+			return err
+		}
+		result.ToEntry,err:=q.CreateEntry(ctx,CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err!=nil{
+			return err
+		}
+		// todo update
+		
+		
+		return nil
+	})
+	return  result
+}

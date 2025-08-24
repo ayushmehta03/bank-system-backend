1a6db9167ca8b28bc308b528f2c4185da81bba17
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-24 17:42:25 +0530

    db deadlock

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,6 +12,7 @@ func TestTransferTx(t *testing.T){
 	store:=NewStore(testDB)
 	account1:=createRandomAccount(t)
 	account2:=createRandomAccount(t)
+	fmt.Println(">> before:",account1.Balance,account2.Balance)
 
 	n:=5
 	amount:=int64(10)
@@ -31,6 +33,7 @@ func TestTransferTx(t *testing.T){
 
 		}()
 	}
+	existed:=make(map[int]bool)
 	for i:=0;i<n;i++{
 		err:=<-errs
 		require.NoError(t,err)
@@ -77,7 +80,40 @@ func TestTransferTx(t *testing.T){
 		// check account balance
 
 		// todo
-		
+
+		// check accounts
+
+		fromAccount:=result.FromAccount
+		require.NotEmpty(t,fromAccount)
+		require.Equal(t,account1.ID,fromAccount.ID)
+
+
+		toAccount:=result.ToAccount
+		require.NotEmpty(t,toAccount.ID)
+		require.Equal(t,account2.ID,toAccount.ID)
+
+
+		// check account balance
+			fmt.Println(">> tx:",fromAccount.Balance,toAccount.Balance)
+
+		diff1:=account1.Balance-fromAccount.Balance
+		diff2:=toAccount.Balance-account2.Balance
+		require.Equal(t,diff1,diff2)
+		require.True(t,diff1>0)
+		require.True(t,diff1%amount==0)
+
+		k:=int(diff1/amount)
+		require.True(t,k>=1&&k<=n)
+
+		require.NotContains(t,existed,k)
+		existed[k]=true
+
+
+
+
+
+
+
 
 
 		
@@ -85,6 +121,18 @@ func TestTransferTx(t *testing.T){
 
 
 	}
+
+	updatedAccount1,err:=testQueries.GetAccount(context.Background(),account1.ID)
+	require.NoError(t,err)
+
+	updatedAccount2,err:=testQueries.GetAccount(context.Background(),account2.ID)
+	require.NoError(t,err)
+	
+		fmt.Println(">> after:",updatedAccount1.Balance,updatedAccount2.Balance)
+
+	require.Equal(t,account1.Balance-int64(n)*amount,updatedAccount1.Balance)
+
+	require.Equal(t,account2.Balance+int64(n)*amount,updatedAccount2.Balance)
 
 
 

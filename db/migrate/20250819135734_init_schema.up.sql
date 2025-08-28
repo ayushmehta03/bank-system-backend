58100bacfffc716805be307133cbd3300cbbe5af
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-28 11:14:53 +0530

    deadlock fixed

--- db/migrate/20250819135734_init_schema.up.sql
+++ db/migrate/20250819135734_init_schema.up.sql
@@ -28,6 +28,11 @@ CREATE TABLE transfers (
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+CREATE INDEX ON "accounts" ("owner");
+CREATE INDEX ON "entries" ("account_id");
 CREATE INDEX ON transfers (from_account_id);
 CREATE INDEX ON transfers (to_account_id);
 CREATE INDEX ON transfers (from_account_id, to_account_id);
